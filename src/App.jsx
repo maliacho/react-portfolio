4a6ef0b9ef4208fa962249d11368e00ea0963ce0
maliacho <maliacho21@gmail.com>
2023-10-26 15:01:35 -0700

    attempting to debug why bootstrap isn't being imported properly

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Header from './components/Header'
+import Project from './components/Project'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Project />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navigation from './Navigation'
+
+function Header() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+export default Header
