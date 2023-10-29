440adb5fa8877966a08e3afdd8ae7b80e4113df2
maliacho <maliacho21@gmail.com>
2023-10-28 22:56:06 -0700

    updated styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,11 @@ const About = () => {
     return (
         <div>
             <div className="card-body border border-black rounded m-5">
-                <h5 className="card-title fs-3 fw-bold text-center border border-black m-3">ABOUT ME</h5>
+                <h5 className="card-title fs-3 fw-bold fst-italic text-center border border-black m-3">ABOUT ME</h5>
                 <p className="card-text m-3">
                     Hi I'm Malia, welcome to my portfolio! I started my web development journey in August 2023, and recently graduated from UCI's The Coding Bootcamp. It has been so fun to learn how to code, and there is still so much for me to learn. I'm happy you're here! Here are a few things to know about me: I drink too much iced coffee, I love Kpop, I go to a lot of concerts, and I will ask to see pictures of your pets.
                 </p>
+                {/* <img src={me} className="card-img-top" alt="..."/> */}
             </div>
         </div>
     )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,22 +3,23 @@ import filmfinder from '../assets/filmfinder.png';
 
 const Project = () => {
     return (
-        <div>
-            <div className="card mb-3 border border-black m-5">
-                <img src={TechView} className="card-img-top" alt="screenshot of ticket home page" />
-                <div className="card-body text-center">
-                    <h5 className="card-title fs-4 fw-bold">IT HELP DESK</h5>
-                    <p className="card-text">This application is designed for employees within a company to be able to
-                        submit a ticket for help from the IT department.</p>
-                    <a className="card-text link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="#"><small className="text-body-secondary text-black">Click Here</small></a>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4 text-center">
+            <div className="row row-cols-1 row-cols-md-2 g-4 d-inline-flex p-2 text-center">
                 <div className="col">
                     <div className="card m-3">
-                        <img src={filmfinder} className="card-img-top" alt="..."/>
+                        <img src={TechView} className="card-img-top" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title fw-bold">FILM FINDER</h5>
+                                <h5 className="card-title fw-bold fst-italic">IT HELP DESK</h5>
+                                <p className="card-text">This application is designed for employees within a company to be able to
+                        submit a ticket for help from the IT department.</p>
+                            <a className="card-text link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="#"><small className="text-body-secondary text-black">Click Here</small></a>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                <div className="card m-3">
+                        <img src={filmfinder} className="card-img-top pt-4 p-5" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title fw-bold fst-italic">FILM FINDER</h5>
                                 <p className="card-text">With Film Finder, you can quickly and effortlessly search for any movie you
                             have in mind. We provide you with all the essential information you need to make an informed
                             decision.</p>
@@ -30,16 +31,7 @@ const Project = () => {
                     <div className="card">
                         <img src="..." className="card-img-top" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
+                                <h5 className="card-title fw-bold fst-italic">Card title</h5>
                                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
                             </div>
                     </div>
@@ -48,13 +40,12 @@ const Project = () => {
                     <div className="card">
                         <img src="..." className="card-img-top" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
+                                <h5 className="card-title fw-bold fst-italic">Card title</h5>
                                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
